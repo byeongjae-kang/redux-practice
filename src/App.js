51033ce175393b047d69fbf0ca4b6bd54e9186be
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-10 14:23:43 -0700

    render auth and userprofile depending on user login status

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import Counter from './components/Counter';
-
+import { Fragment } from "react";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
+import { useSelector } from "react-redux";
 
 function App() {
+  const auth = useSelector((state) => state.auth.isAuthentificated);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {auth && <UserProfile />}
+      {!auth && <Auth />}
+      <Counter />
+    </Fragment>
   );
 }
 
